631b87c2253b04b897594934a6d18ad5237cda9c
0xanonymeow <anonymeow@incatni.to>
2023-05-20 15:50:55 +0700

    style: format

---
+++ components/Terrain.tsx
@@ -0,0 +1,26 @@
+import { Plane } from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three'
+
+export const Terrain = () => {
+  const [height, texture, specular] = useLoader(TextureLoader, [
+    '/assets/images/tiles/depth_z1.jpg',
+    '/assets/images/tiles/terrain_z1.jpg',
+    '/assets/images/tiles/ocean_z1.jpg',
+  ])
+
+  return (
+    <mesh>
+      <Plane args={[8, 8, 1000, 1000]}>
+        <meshPhongMaterial
+          attach="material"
+          map={texture}
+          displacementMap={height}
+          displacementScale={1}
+          specularMap={specular}
+          shininess={500}
+        />
+      </Plane>
+    </mesh>
+  )
+}
