74f9e4e80a6cc61cfac554a0d7881a7b4492817f
0xanonymeow <anonymeow@incatni.to>
2023-05-21 12:05:39 +0700

    fix: update camera control

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,13 +1,12 @@
 import { OrbitControls } from '@react-three/drei'
-import { useThree } from '@react-three/fiber'
-import { useEffect, useRef, useState } from 'react'
-import { MOUSE, Vector3 } from 'three'
+import { useFrame, useThree } from '@react-three/fiber'
+import { forwardRef, useEffect, useState } from 'react'
+import { MOUSE, Quaternion } from 'three'
 
-export const Controls = ({ isLoggedIn }) => {
-  const controls = useRef()
+const Controls = forwardRef(({ isLoggedIn }, ref) => {
   const { camera, gl } = useThree()
   const [lastValidMaxDistance, setLastValidMaxDistance] = useState(3) // Initial value
-  const minDistance = 1.2
+  const minDistance = 0.6
 
   const calculateMaxDistance = () => {
     const vFOV = (camera.fov * Math.PI) / 180 // convert vertical fov to radians
@@ -22,102 +21,33 @@ export const Controls = ({ isLoggedIn }) => {
   }
 
   useEffect(() => {
-    if (!controls.current || !controls.current.target || !isLoggedIn) return
-
-    const getZoomLevel = () => {
-      return camera.position.distanceTo(controls.current.target)
-    }
-
-    const smoothness = 0.1 // 0 to 1 only
-    let targetPosition = controls.current.target.clone()
-
-    const zoom = (event) => {
-      const currentZoomLevel = getZoomLevel()
-
-      let direction = new Vector3(0, 0, 0)
-      if (event.deltaY < 0) {
-        // scrolling up, zoom in
-        direction = new Vector3(0, 0, -1) // negative to move closer
-      } else if (event.deltaY > 0) {
-        // scrolling down, zoom out
-        direction = new Vector3(0, 0, 1)
-      }
-
-      // calculate potential target position
-      let potentialTarget = new Vector3().addVectors(
-        camera.position,
-        direction.multiplyScalar(Math.abs(event.deltaY) * 0.01) // adjust zoom speed here
-      )
-
-      // check if the potential target position is within the valid range
-      let distance = controls.current.target.distanceTo(potentialTarget)
-      if (distance < minDistance) {
-        // if too close, adjust it to be exactly at the min distance
-        let direction = potentialTarget
-          .clone()
-          .sub(controls.current.target)
-          .normalize()
-        targetPosition = new Vector3().addVectors(
-          controls.current.target,
-          direction.multiplyScalar(minDistance)
-        )
-      } else if (distance > lastValidMaxDistance) {
-        // if too far, adjust it to be exactly at the max distance
-        let direction = potentialTarget
-          .clone()
-          .sub(controls.current.target)
-          .normalize()
-        targetPosition = new Vector3().addVectors(
-          controls.current.target,
-          direction.multiplyScalar(lastValidMaxDistance)
-        )
-      } else {
-        // if within valid range, set it as the target position
-        targetPosition = potentialTarget
-      }
-
-      if (currentZoomLevel <= 1.8) {
-        targetPosition.y = -0.25
-      } else if (currentZoomLevel <= 1.5) {
-        targetPosition.y = -0.5
-      } else if (currentZoomLevel > 1.5) {
-        targetPosition.y = 0
-      }
-
-      camera.position.lerp(targetPosition, smoothness)
-    }
-
-    window.addEventListener('wheel', zoom)
-
-    return () => window.removeEventListener('wheel', zoom)
-  }, [
-    isLoggedIn,
-    camera.position,
-    controls.current,
-    camera.fov,
-    lastValidMaxDistance,
-  ])
-
-  useEffect(() => {
     calculateMaxDistance()
     window.addEventListener('resize', calculateMaxDistance)
 
     // Clean up when the component unmounts
     return () => window.removeEventListener('resize', calculateMaxDistance)
-  }, [camera, controls])
+  }, [camera, ref])
+
+  useFrame(() => {
+    if (camera.position.z < 1) {
+      const quaternion = new Quaternion(0.5, 0, 0, 1)
+      camera.applyQuaternion(quaternion) // Apply Quaternion
+      camera.quaternion.normalize() // Normalize Quaternion
+    }
+  })
 
   return (
     <OrbitControls
-      ref={controls}
+      ref={ref}
       minDistance={minDistance}
       maxDistance={lastValidMaxDistance}
-      // maxPolarAngle={-0.5}
-      dampingFactor={0.1}
+      minAzimuthAngle={0}
+      maxAzimuthAngle={0}
+      dampingFactor={0.2}
       autoRotate={false}
       enableRotate={false}
-      enableZoom={false}
       enablePan={isLoggedIn}
-      zoomSpeed={0.8}
+      zoomSpeed={0.9}
       mouseButtons={{
         LEFT: MOUSE.PAN,
       }}
@@ -125,4 +55,8 @@ export const Controls = ({ isLoggedIn }) => {
       enableDamping
     />
   )
-}
+})
+
+Controls.displayName = 'Controls'
+
+export { Controls }
