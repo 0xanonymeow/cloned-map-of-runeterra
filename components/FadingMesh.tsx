df23e7cd069dea0253ccb54388189f206dcee72a
0xanonymeow <anonymeow@incatni.to>
2023-05-21 09:18:24 +0700

    fix: update fadingMesh logic

--- components/FadingMesh.tsx
+++ components/FadingMesh.tsx
@@ -8,12 +8,14 @@ export const FadingMesh = ({ meshRef, fade }) => {
     if (meshRef.current.material.opacity > 0) {
       meshRef.current.material.opacity -= 0.01
       meshRef.current.material.needsUpdate = true
+    } else {
+      meshRef.current.material.needsUpdate = false
     }
   })
 
   return (
     <mesh ref={meshRef}>
-      <boxGeometry args={[7.8, 7.8, 1]} />
+      <boxGeometry args={[7.8, 7.8, fade ? 0 : 1]} />
       <meshBasicMaterial
         transparent
         opacity={0.5}
