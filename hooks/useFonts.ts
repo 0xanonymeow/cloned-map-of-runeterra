54dba0a70dfe32fe19a42420a8b855dbf0db0494
0xanonymeow <anonymeow@incatni.to>
2023-05-21 08:05:24 +0700

    feat: add useFonts

--- hooks/useFonts.ts
+++ hooks/useFonts.ts
@@ -2,7 +2,21 @@ import localFont from 'next/font/local'
 const BeaufortforLOL = localFont({
   src: '../public/assets/fonts/BeaufortforLOL-Bold.ttf',
 })
+const Spiegel = localFont({
+  src: [
+    {
+      path: '../public/assets/fonts/Spiegel-Bold.ttf',
+      style: 'normal',
+      weight: '700',
+    },
+    {
+      path: '../public/assets/fonts/Spiegel-Regular.ttf',
+      style: 'normal',
+      weight: '400',
+    },
+  ],
+})
 
 export const useFonts = () => {
-  return { BeaufortforLOL }
+  return { BeaufortforLOL, Spiegel }
 }
