e08ba3d1fce894e06beceff7d196f800e57f12ab
0xanonymeow <anonymeow@incatni.to>
2023-05-22 17:07:58 +0700

    fix: remove inconsistent maxDistance calculation

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,33 +1,12 @@
 import { OrbitControls } from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
-import { forwardRef, useEffect, useState } from 'react'
+import { forwardRef } from 'react'
 import { MOUSE, Quaternion } from 'three'
 
 const Controls = forwardRef(({ isLoggedIn }: { isLoggedIn: boolean }, ref) => {
   const { camera, gl } = useThree()
-  const [lastValidMaxDistance, setLastValidMaxDistance] = useState(3) // Initial value
   const minDistance = 0.7
-
-  const calculateMaxDistance = () => {
-    // @ts-ignore
-    const vFOV = (camera.fov * Math.PI) / 180 // convert vertical fov to radians
-    const ratio = 2 * Math.tan(vFOV / 2) // visible height
-    const screen = ratio * (window.innerWidth / window.innerHeight) // visible width
-
-    // Use the size of the plane
-    const size = 7.8
-
-    if (window.innerWidth >= window.innerHeight)
-      setLastValidMaxDistance(size / screen)
-  }
-
-  useEffect(() => {
-    calculateMaxDistance()
-    window.addEventListener('resize', calculateMaxDistance)
-
-    // Clean up when the component unmounts
-    return () => window.removeEventListener('resize', calculateMaxDistance)
-  }, [camera, ref])
+  const maxDistance = 3
 
   useFrame(() => {
     if (camera.position.z < 1) {
@@ -42,7 +21,7 @@ const Controls = forwardRef(({ isLoggedIn }: { isLoggedIn: boolean }, ref) => {
       // @ts-ignore
       ref={ref}
       minDistance={minDistance}
-      maxDistance={lastValidMaxDistance}
+      maxDistance={maxDistance}
       minAzimuthAngle={0}
       maxAzimuthAngle={0}
       dampingFactor={0.2}
